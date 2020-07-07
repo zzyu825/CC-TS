2a89b1ff8b596af31a7ae6ac3a2b1cb401f3e17d
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-07 17:46:23 +0800

    2-01

--- index.ts
+++ index.ts
@@ -1,14 +1,1 @@
-function getUserName():string | number {
-    if (Math.random() < 0.5) {
-        return 'zheng zhi yu'
-    }
-    return 404;
-}
-
-let myname = getUserName();
-if (typeof myname === "string") {
-    myname = myname.split('')
-        .filter(it => it)
-        .map(it => it[0].toUpperCase() + it.substr(1))
-        .join(" ");
-}
+let a:string = 'ts';
