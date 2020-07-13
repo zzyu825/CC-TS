a69247886337947a2a4b254596c364849f3315e2
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-13 18:13:26 +0800

    3-01

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-let a:string = 'ts';
+let name: string;
+// name = 123;
 
-console.log(a); 
+function add(a: number, b: number): number {
+    return a + b;
+}
+// sum('1', '2');
+// sum = 3;
+// 改函数名 fn + F2 -> add
+// let num: string = sum(1, 2);
+
+// 假设在另一个文件中
+// 跳转到函数定义位置 fn + F12
+// let num: number = add(1, 2);
+
+// 类型推导
+// function add(a: number, b: number) {
+//     return a + b;
+// }
+// let num = add(1, 2);
+
+// let name = 'abc';
+// name = 123;
+
+// 区分数字字符串和数字
+// let age = 12;
+// let phone = '13523456789';
