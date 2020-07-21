df98997138be49b24baf50f0d8134d8a21b90800
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-21 09:00:19 +0800

    3-03

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
-function isOdd(n: number) {
-    return n % 2 === 0;
+let name: string | undefined; // 联合类型
+
+function fn(): void { // 函数无任何返回值
+    if (typeof name === 'string') { // 类型保护
+        name.toUpperCase();
+    }
 }
 
-// let arr: number[];
-// let arr = [1, 2, 3];
-let arr: Array<number>;
-
-// let obj: object;
-// obj = '123';
-
-function printValues(obj: object) {
-    const vals = Object.values(obj);
-    vals.forEach(v => console.log(v));
+function throwError(msg: string): never { // 函数永远不会结束
+    throw new Error(msg);
 }
-printValues({
-    name: 'a',
-    age: 1
-});
 
-// let n: string = undefined;
-// n.toUpperCase();
+let gender: '男' | '女'; // 字面量类型
+gender = '男';
+
+let arr: []; // arr永远只能取值为一个空数组
+arr = [];
+
+let user: {
+    name: string
+    age: number
+}
+user = {
+    name: 'abc',
+    age: 12
+}
+
+let arr1: [string, number]; // 元组类型
+arr1 = ['1', 1];
+
+let a: any = 1;
