9e18ea62180dd90cd5505479cf5c6b7a091aafc5
zhengzhiyu <zhengzhiyu825@163.com>
2020-08-04 09:59:59 +0800

    3-04

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,18 @@
-let name: string | undefined; // 联合类型
-
-function fn(): void { // 函数无任何返回值
-    if (typeof name === 'string') { // 类型保护
-        name.toUpperCase();
-    }
+type Gender = "男" | "女";
+type User = {
+    name: String,
+    gender: Gender,
+    age: Number
 }
 
-function throwError(msg: string): never { // 函数永远不会结束
-    throw new Error(msg);
+let u:User;
+u = {
+    name: 'zzyu',
+    gender: '男',
+    age: 18
 }
 
-let gender: '男' | '女'; // 字面量类型
-gender = '男';
-
-let arr: []; // arr永远只能取值为一个空数组
-arr = [];
-
-let user: {
-    name: string
-    age: number
+function getUsers(g:Gender):User[] {
+    return [];
 }
-user = {
-    name: 'abc',
-    age: 12
-}
-
-let arr1: [string, number]; // 元组类型
-arr1 = ['1', 1];
-
-let a: any = 1;
+getUsers("男");
