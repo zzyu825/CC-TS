1e140ae56c63a0764d1e89c188f3b40ba23ef798
zhengzhiyu <zhengzhiyu825@163.com>
2020-08-10 15:45:23 +0800

    5-01

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-enum Permission {
-    Read = 1, // 0001
-    Write = 2, // 0010
-    Create = 4, // 0100
-    Delete = 8 // 1000
-}
+import { name, sum } from "./myModule";
 
-// 1.如何组合权限
-// 使用或运算
-let p = Permission.Read | Permission.Write;
-
-// 2.如何判断是否拥有某个权限
-// 0011且0010
-function hasPermission(target: Permission, per: Permission) {
-    return (target & per) === per;
-}
-// 判断变量p是否拥有可读权限
-console.log(hasPermission(p, Permission.Delete));
-
-// 3.如何删除某个权限
-// 0011异或0010
-p = p ^ Permission.Write;
-console.log(hasPermission(p, Permission.Write));
+console.log(name);
+console.log(sum(1, 2));
---
+++ src/myModule.ts
@@ -0,0 +1,6 @@
+export const name = 'zzyu';
+export function sum(a:number, b:number) {
+    return a + b;
+}
+
+// 使用默认导出，在导入时没有智能提示
