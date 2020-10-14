b293f2d2c78adcb0ccd7d5949f80a17bd8e26c9f
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-14 11:32:42 +0800

    5-04

--- note.md
+++ note.md
@@ -28,3 +28,9 @@ TS中的模块化在编译结果中：
 
 - 如果编译结果的模块化标准是ES6： 没有区别
 - 如果编译结果的模块化标准是commonjs：导出的声明会变成exports的属性，默认的导出会变成exports的default属性；
+
+# 如何在TS中书写commonjs模块化代码
+
+导出：export = xxx
+
+导入：import xxx = require("xxx")
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-// import fs from 'fs'; // module.exports={}
-// import { readFileSync } from 'fs';
-// import * as fs from 'fs';
-import fs from 'fs';
-
-// readFileSync('/');
-fs.readFileSync('/');
-
-let a:string = 123;
+import obj = require('./myModule');
+console.log(obj.name);
