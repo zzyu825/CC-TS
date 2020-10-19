c2f723b02c4f88b4fd9d35fadc476735a6ae56f2
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-19 14:45:36 +0800

    6-03

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,26 @@
 // interface User {
+//   readonly id: number,
 //   name: string,
-//   age: number,
-//   sayHello(): void
+//   age: number
 // }
 
-import { type } from "os"
+type User = {
+  readonly id: number,
+  name: string,
+  age: number,
+  readonly arr: readonly string[]
+}
 
-// type User = {
-//   name: string,
-//   age: number,
-//   sayHello: () => void
-// }
+let u: User = {
+  id: 1,
+  name: 'a',
+  age: 18,
+  arr: ['1']
+}
 
-// let u: User = {
-//   name: 'a',
-//   age: 18,
-//   sayHello() {
-//     console.log('1');
-//   }
-// }
+// u.id = 2;
+// u.arr = 
 
-// type Condition = (n: number) => boolean
-// type Condition = { // 界定符
-//   (n: number): boolean
-// }
-
-// interface Condition {
-//   (n: number): boolean
-// }
-
-// function sum(numbers: number[], callback: Condition) {
-//   let s = 0;
-//   numbers.forEach(n => {
-//     if (callback(n)) {
-//       s += n;
-//     }
-//   });
-//   return s;
-// }
-
-// const result = sum([1,2,3,4,5], n => n % 2 !== 0);
-// console.log(result);
-
-// interface A {
-//   T1: string
-// }
-
-// interface B {
-//   T2: number
-// }
-
-// interface C extends A, B {
-//   T3: boolean
-// }
-
-// type A = {
-//   T1: string
-// }
-
-// type B = {
-//   T2: number
-// }
-
-// type C = {
-//   T1: number,
-//   T3: boolean
-// } & A & B
-
-// let u: C = {
-//   T1: '1',
-//   T2: 2,
-//   T3: true
-// }
+// const arr:readonly number[] = [1,2,3];
+// const arr: ReadonlyArray<number> = [1,2,3];
+// arr.push()
