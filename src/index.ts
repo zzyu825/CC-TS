8226db3763b3b01e7a1084eb4b22cac9b791ca30
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-21 19:45:20 +0800

    6-04

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-// interface User {
-//   readonly id: number,
-//   name: string,
-//   age: number
+interface Duck {
+  sound: "嘎嘎嘎",
+  swin(): void
+}
+
+let person = {
+  name: "伪装成鸭子的人",
+  age: 11,
+  sound: "嘎嘎嘎" as "嘎嘎嘎",
+  swin() {
+    console.log(this.name + "正在游泳，并发出了" + this.sound +  "的声音")
+  }
+}
+
+// let duck: Duck = person;
+// let duck: Duck = {
+//   sound: "嘎嘎嘎" as "嘎嘎嘎",
+//   swin() {
+//     console.log(this.name + "正在游泳，并发出了" + this.sound +  "的声音")
+//   }
 // }
 
-type User = {
-  readonly id: number,
-  name: string,
-  age: number,
-  readonly arr: readonly string[]
-}
-
-let u: User = {
-  id: 1,
-  name: 'a',
-  age: 18,
-  arr: ['1']
-}
-
-// u.id = 2;
-// u.arr = 
-
-// const arr:readonly number[] = [1,2,3];
-// const arr: ReadonlyArray<number> = [1,2,3];
-// arr.push()
+// interface User {
+//   name?: string
+//   age: number
+// }
+// let u: User = {
+//   nema: 'a',
+//   age: 18
+// }
