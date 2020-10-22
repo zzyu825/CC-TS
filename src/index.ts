2e80e1ed3e175f7e1871f3e32ee9ccc276b4cff8
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-22 20:20:14 +0800

    7-03

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 class User {
-  age: number
   gender: "男" | "女" = "男"
   pid?: string
   readonly id: number
 
-  private publishNumber: number = 3 // 每天可以发布的文章数量
-  private curNumber: number = 0 // 当前可以发布的文章数量
+  private _publishNumber: number = 3 // 每天可以发布的文章数量
+  private _curNumber: number = 0 // 当前可以发布的文章数量
 
-  constructor(public name: string, age: number) {
+  constructor(public name: string, private _age: number) {
     this.id = Math.random();
-    this.age = age
+  }
+
+  // setAge() {}
+  // getAge() {}
+
+  set age(value: number) {
+    // 条件控制
+    this._age = value
+  }
+
+  get age() {
+    return Math.floor(this._age)
   }
 
   publish(title: string) {
-    if (this.curNumber < this.publishNumber) {
+    if (this._curNumber < this._publishNumber) {
       console.log("发布一篇文章：" + title);
-      this.curNumber++;
+      this._curNumber++;
     } else {
       console.log("你今日发布的文章数量已达上限")
     }
