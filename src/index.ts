1e467986b52be8645a2406d5a9df0182a72cb60c
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-26 19:52:25 +0800

    8-05

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-/**
- * 将两个数组进行混合
- * @param arr1 
- * @param arr2 
- */
-function mixinArray<T, K>(arr1: T[], arr2: K[]): (T | K)[] {
-  if (arr1.length !== arr2.length) {
-    throw new Error("两个数组长度不等");
-  }
-  let result: (T | K)[] = [];
-  for (let i = 0; i < arr1.length; i++) {
-    result.push(arr1[i]);
-    result.push(arr2[i]);
-  }
-  return result;
-}
+import { Dictionary } from "./dictionary";
 
-mixinArray([1, 2, 3], ['a', 'b', 'c']);
+const dic = new Dictionary<string, number>();
+
+dic.set("a", 1);
+dic.set("b", 2);
+
+dic.forEach((k, v) => {
+  console.log(`${k}:${v}`);
+});
+
+console.log(dic.has('a'));
